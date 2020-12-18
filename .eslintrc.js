3163ececb63f795a8f522dbabadb7ded3d739ea6
Autapomorph <Autapomorph@users.noreply.github.com>
2020-12-18 12:07:22 +0300

    chore(deps): bump deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     node: true,
   },
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
   rules: {
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
--- webpack/prod.js
+++ webpack/prod.js
@@ -5,96 +5,93 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const OfflinePlugin = require('offline-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const commonConfig = require('./common');
 
 process.env.NODE_ENV = 'production';
 
-module.exports = (env = {}) =>
-  mergeWithCustomize({
-    customizeArray(a, b, key) {
-      // Use b's rule for /\.css$/
-      if (key === 'module.rules') {
-        const findCSSRuleIndex = rules =>
-          rules.findIndex(rule => String(rule.test) === String(/\.css$/));
-        a.splice(findCSSRuleIndex(a), 1, b[findCSSRuleIndex(b)]);
-        return a;
-      }
+module.exports = mergeWithCustomize({
+  customizeArray(a, b, key) {
+    // Use b's rule for /\.css$/
+    if (key === 'module.rules') {
+      const findCSSRuleIndex = rules =>
+        rules.findIndex(rule => String(rule.test) === String(/\.css$/));
+      a.splice(findCSSRuleIndex(a), 1, b[findCSSRuleIndex(b)]);
+      return a;
+    }
 
-      // Fall back to default merging
-      return undefined;
-    },
-  })(commonConfig, {
-    mode: 'production',
-    output: {
-      filename: '[name].[contenthash].js',
-      chunkFilename: '[name].[contenthash].js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-        },
-      ],
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new CopyPlugin({
-        patterns: [
-          {
-            from: 'assets',
-            to: 'assets',
-            globOptions: {
-              ignore: ['**/_raw/**', '**/*.md'],
-            },
-          },
-        ],
-      }),
-      new MiniCssExtractPlugin({
-        filename: '[name].[contenthash].css',
-        chunkFilename: '[name].[contenthash].css',
-      }),
-      new OfflinePlugin({
-        responseStrategy: 'network-first',
-        caches: {
-          main: [':rest:', 'favicons/favicon*.*'],
-          additional: [':externals:'],
-          optional: ['favicons/**/*'],
-        },
-        safeToUseOptionalCaches: true,
-        ServiceWorker: {
-          events: true,
-        },
-      }),
-      new BundleAnalyzerPlugin({ analyzerMode: env.analyze ? 'server' : 'disabled' }),
-    ],
-    optimization: {
-      moduleIds: 'hashed',
-      runtimeChunk: true,
-      splitChunks: {
-        chunks: 'all',
+    // Fall back to default merging
+    return undefined;
+  },
+})(commonConfig, {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
       },
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            output: {
-              comments: false,
-            },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'assets',
+          to: 'assets',
+          globOptions: {
+            ignore: ['**/_raw/**', '**/*.md'],
           },
-          extractComments: false,
-        }),
-        new OptimizeCSSAssetsPlugin(),
+        },
       ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[name].[contenthash].css',
+    }),
+    new OfflinePlugin({
+      responseStrategy: 'network-first',
+      caches: {
+        main: [':rest:', 'favicons/favicon*.*'],
+        additional: [':externals:'],
+        optional: ['favicons/**/*'],
+      },
+      safeToUseOptionalCaches: true,
+      ServiceWorker: {
+        events: true,
+      },
+    }),
+  ],
+  optimization: {
+    moduleIds: 'hashed',
+    runtimeChunk: true,
+    splitChunks: {
+      chunks: 'all',
     },
-    performance: {
-      maxEntrypointSize: 1 * 1024 * 1024,
-      maxAssetSize: 1 * 1024 * 1024,
-    },
-    stats: {
-      all: false,
-      assets: true,
-      excludeAssets: [/assets/, /favicons/],
-    },
-  });
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+      new OptimizeCSSAssetsPlugin(),
+    ],
+  },
+  performance: {
+    maxEntrypointSize: 1.1 * 1024 * 1024,
+    maxAssetSize: 1 * 1024 * 1024,
+  },
+  stats: {
+    all: false,
+    assets: true,
+    excludeAssets: [/assets/, /favicons/],
+  },
+});
